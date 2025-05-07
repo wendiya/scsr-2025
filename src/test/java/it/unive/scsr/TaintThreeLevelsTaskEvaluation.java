69dfcb19a7c907f0ac6ba78a016d2a747552698c
Luca Olivieri <oli.luca93@gmail.com>
2025-05-07 16:39:41 +0300

    Added expected results for TaintThreeLevels and Intervals tests

--- src/test/java/it/unive/scsr/TaintThreeLevelsTaskEvaluation.java
+++ src/test/java/it/unive/scsr/TaintThreeLevelsTaskEvaluation.java
@@ -1,23 +1,36 @@
 package it.unive.scsr;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
 import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
-import it.unive.lisa.analysis.taint.ThreeLevelsTaint;
+import it.unive.scsr.TaintThreeLevels;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.conf.LiSAConfiguration.GraphType;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
 import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.interprocedural.context.FullStackToken;
+import it.unive.lisa.outputs.compare.JsonReportComparer;
+import it.unive.lisa.outputs.json.JsonReport;
 import it.unive.lisa.program.ClassUnit;
 import it.unive.lisa.program.Program;
 import it.unive.lisa.program.Unit;
 import it.unive.lisa.program.cfg.CodeMember;
 import it.unive.lisa.program.cfg.Parameter;
+import it.unive.lisa.util.file.FileManager;
 import it.unive.scsr.checkers.TaintThreeLevelsChecker;
 
 public class TaintThreeLevelsTaskEvaluation {
@@ -54,7 +67,7 @@ public class TaintThreeLevelsTaskEvaluation {
 		
 		 conf.abstractState = DefaultConfiguration.simpleState(
 				DefaultConfiguration.defaultHeapDomain(),
-				new ValueEnvironment<>(new ThreeLevelsTaint()),
+				new ValueEnvironment<>(new TaintThreeLevels()),
 				DefaultConfiguration.defaultTypeDomain());
 		 
 		 // we specify to perform an interprocedural analysis (require to recognize calls to sources, sanitizers, and sinks)
@@ -63,12 +76,41 @@ public class TaintThreeLevelsTaskEvaluation {
 		 // the TaintChecker is executed after the taint analysis and it checks if a tainted value is flowed in a sink
 		 conf.semanticChecks.add(new TaintThreeLevelsChecker());
 		 
+		conf.serializeResults = true;
+		conf.jsonOutput = true;
+		
+		try {
+			FileManager.forceDeleteFolder(conf.workdir);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			fail("Cannot delete working directory '" + conf.workdir + "': " + e.getMessage());
+		}
+		 
 		// we instantiate LiSA with our configuration
 		LiSA lisa = new LiSA(conf);
 		
 
 		// finally, we tell LiSA to analyze the program
 		lisa.run(program);
+		
+
+		Path expectedPath = Paths.get("expected", "taint-3lvs-eval");
+		Path actualPath = Paths.get("outputs", "taint-3lvs-eval");
+
+		File expFile = Paths.get(expectedPath.toString(), "report.json").toFile();
+		File actFile = Paths.get(actualPath.toString(), "report.json").toFile();
+		try {
+			JsonReport expected = JsonReport.read(new FileReader(expFile));
+			JsonReport actual = JsonReport.read(new FileReader(actFile));
+			assertTrue("Results are different",
+					JsonReportComparer.compare(expected, actual, expectedPath.toFile(), actualPath.toFile()));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace(System.err);
+			fail("Unable to find report file");
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			fail("Unable to compare reports");
+		}
 	}
 
 
